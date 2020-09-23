d2725c25049b7e2c7176a77488ef162ae7ffdda7
alexoid <alex_zam@hotmail.es>
2020-09-23 12:58:38 -0500

    add navbar and error mesages helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def error_flashes
+        if flash[:notice]
+          content_tag(:p, flash[:notice], class: 'alert alert-success')
+        elsif flash[:alert]
+          content_tag(:p, flash[:alert], class: 'alert alert-danger')
+        end
+    end
 end
