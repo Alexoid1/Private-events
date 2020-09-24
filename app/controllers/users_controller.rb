970b009f3dcc2f83767dccafc5bae6aa70ef2eeb
alexoid <alex_zam@hotmail.es>
2020-09-23 23:49:33 -0500

    add sign in sign out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,22 @@
 class UsersController < ApplicationController
+    def index
+        @users=User.all
+    end    
+    
     def new
         @user = User.new
     end
       
     def create
-        @user = User.new(params[:user])
+        @user = User.new(user_params)
         if @user.save
           session[:user_id] = @user.id
-          redirect_to root_url, notice: "Thank you for signing up!"
+          redirect_to events_path, notice: "Thank you for signing up!"
         else
           render "new"
         end
+    end
+    def user_params
+        params.require(:user).permit(:name, :email)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_23_203705) do
+ActiveRecord::Schema.define(version: 2020_09_23_224958) do
 
   create_table "events", force: :cascade do |t|
     t.text "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_09_23_203705) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
 end
